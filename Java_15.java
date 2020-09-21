8bb9c037a99d6bb32ec11855edb90d131b51f0da
xiaolong9420 <65012037+xiaolong9420@users.noreply.github.com>
2020-09-21 13:35:56 +0800

    报数游戏
    一共有n个人，报到x的人出去

--- Java_15.java
+++ Java_15.java
@@ -7,21 +7,31 @@ public class Java_15 {
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
 		Scanner in = new Scanner(System.in);
-		int n=7;
-		int arr[] =new int [n];
-		int k=0,i=1;
-		while(k<n)
-		{	
-			i++;
-			if(i%3==0) {
-				arr[i]=1;
-			k++;
+		System.out.println("请输入一共有几个人：");
+		int n = in.nextInt();
+		System.out.println("请输入报到几的人出去");
+		int x = in.nextInt();
+		int arr[] = new int[n];
+		int k = 0, m = 0;		//k 出去几个人， m 判断报的数是否为x 的倍数
+		while (k < n - 1) {
+			for (int i = 0; i < arr.length; i++) {
+				m++;		//先计数，后判断
+				if (arr[i] == 1) {
+					m--;		//出去的人减去
+					continue;
+				}
+				if (m % x == 0 && m != 0) {	
+					arr[i] = 1;
+					k++;
+				}
+
 			}
+
 		}
-		for(int j=0;j<arr.length;j++)
-		{
-			if(arr[j]==0) {
-				System.out.println(j);
+
+		for (int j = 0; j < arr.length; j++) {
+			if (arr[j] == 0) {
+				System.out.println("留到最后的是" + (j + 1) + "号");
 			}
 		}
 	}
