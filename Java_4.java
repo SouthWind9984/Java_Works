34a5246e1d6d22bec212ea6caca6bbcc23a12346
xiaolong9420 <65012037+xiaolong9420@users.noreply.github.com>
2020-09-22 20:12:12 +0800

    更新 Java_4.java

--- Java_4.java
+++ Java_4.java
@@ -7,7 +7,7 @@ public class Java_4 {
 		int score=90;         //分数的值可以随便修改
         if(score>=90){    	
             System.out.println("A");
-        }else if(score>60){    	
+        }else if(score>=60){    	
             System.out.println("B");
         }else{    	
             System.out.println("C");
